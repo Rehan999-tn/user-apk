03b9367c8ee70d08174c62fd845525fdd06368bb
Rehan999-tn <jiwakuasu7251@gmail.com>
2025-09-03 18:21:32 +0700

    Update database tokens

--- user.js
+++ user.js
@@ -5,14 +5,5 @@
       "session_1164848674"
     ]
   },
-  "tokens": {
-    "session_1164848674": {
-      "githubToken": "pp",
-      "githubOwner": "Rehan999-tn",
-      "githubRepo": "db-mt",
-      "fileName": "text.json",
-      "telegramToken": "Kontol",
-      "timestamp": "2025-09-03T11:18:32.837Z"
-    }
-  }
+  "tokens": {}
 }
