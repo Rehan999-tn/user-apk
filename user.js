914566ff338fee46cd0be162e2e0a7fb3d14a196
Rehan999-tn <jiwakuasu7251@gmail.com>
2025-09-03 18:37:42 +0700

    Update database tokens

--- user.js
+++ user.js
@@ -5,14 +5,5 @@
       "session_1164848674"
     ]
   },
-  "tokens": {
-    "token_1756899452201": {
-      "githubToken": "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4",
-      "githubOwner": "Rehan999-tn",
-      "githubRepo": "db-mt",
-      "fileName": "text.json",
-      "telegramToken": "tai",
-      "timestamp": "2025-09-03T11:37:32.182Z"
-    }
-  }
+  "tokens": {}
 }
